cd083d38454b82cfd2c2c32113b2e5a074332511
g <diegocaucota@gmail.com>
2025-05-08 12:03:33 -0300

    fix: auth login function

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -1,12 +1,28 @@
 import axios from 'axios';
 
-const API = 'http://localhost:4000/api/auth/login';
+const API = 'http://localhost:4000/api/auth';
 
 export const loginUser = async (email, password) => {
-  const res = await axios.post(API, {
-    email,
-    password,
-  });
+  try {
+    const res = await axios.post(`${API}/login`, {
+      email,
+      password,
+    });
+    return res.data; // { token: "...", user: { ... } }
+  } catch (err) {
+    throw new Error("Credenciales incorrectas", err);
+  }
+};
 
-  return res.data; // { token, user }
+export const getUserData = async (token) => {
+  try {
+    const res = await axios.get(`${API}/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data; // { user: { nombre, email, role } }
+  } catch (err) {
+    throw new Error("Token inválido o expirado", err);
+  }
 };
