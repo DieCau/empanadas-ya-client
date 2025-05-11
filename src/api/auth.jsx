e956bd8cf8cf478b268ee1bc8fcac5bfb638e387
g <diegocaucota@gmail.com>
2025-05-10 23:38:14 -0300

    fix: auth

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = "http://localhost:4000/api/auth";
 
+// Ingresar en la base de datos
 export const loginUser = async (email, password) => {
   try {
     const res = await axios.post(`${API}/login`, {
